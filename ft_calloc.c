91536188a350f163177245d4a854b19005416a22
Javier Andreu <andreujavier99@gmail.com>
2024-05-21 19:42:33 +0200

    ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: antandre <antandre@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 13:44:16 by antandre          #+#    #+#             */
-/*   Updated: 2024/05/21 19:37:16 by antandre         ###   ########.fr       */
+/*   Updated: 2024/05/21 19:42:19 by antandre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 #include <stdlib.h>
 #include "libft.h"
 
-// La función actúa igual que malloc pero añadiendo tres verificaciones de errores e 
-// inicializando los bytes en 0.
-// Si count o size son 0, asignamos 1 byte de memoria para devolver un puntero no nulo
-// que pueda ser pasado a free().
+// La función actúa igual que malloc pero añadiendo tres verificaciones
+// de errores e inicializando los bytes en 0.
+// Si count o size son 0, asignamos 1 byte de memoria para devolver un 
+// puntero no nulo que pueda ser pasado a free().
 // Si hay integer overflow devolvemos un error.
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void *vector;
+	void	*vector;
 
 	vector = malloc(size * count);
 	if (vector == NULL)
